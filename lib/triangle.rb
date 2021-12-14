b9714ef900ce6957a29ef20e5601521d500fc326
Abbigale Bercher <abbeybercher@gmail.com>
2021-12-14 12:10:38 -0500

    commit tree debe5c3541ca2de4c513d9a64f6753e04294dcb3

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,38 @@
 class Triangle
-  # write code here
+  attr_reader :a, :b, :c
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    triangle_validator
+    if a == b && b == c
+      :equilateral
+    elsif a != b && b != c && a != c
+      :scalene
+    else
+      :isosceles
+    end
+
+  end
+
+  def sides_greater_than_zero?
+    [a, b, c].all?(&:positive?)
+  end
+
+  def sum_of_two?
+    a + b > c && a + c > b && b + c > a
+  end
+
+  def triangle_validator
+    raise TriangleError unless sides_greater_than_zero? && sum_of_two?
+  end
+
+  class TriangleError < StandardError
+
+  end
+
 end
